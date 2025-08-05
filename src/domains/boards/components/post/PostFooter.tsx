2146405872ef87cd206c9e3ee1745014e781608e
nine9188 <nine9188@naver.com>
2025-08-05 22:05:35 +0900

    디테일수정

--- src/domains/boards/components/post/PostFooter.tsx
+++ src/domains/boards/components/post/PostFooter.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { ListOrdered, PenLine, Edit, Trash } from 'lucide-react';
+import { PenLine, Edit, Trash } from 'lucide-react';
 import { deletePost } from '@/domains/boards/actions/posts/index';
 import ReportButton from '@/domains/reports/components/ReportButton';
 
