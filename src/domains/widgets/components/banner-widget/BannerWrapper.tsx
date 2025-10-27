f55e7ba68e52ec720b3c41217b32852f18c4b461
nine9188 <nine9188@naver.com>
2025-10-28 02:55:54 +0900

    메인페이지 위젯 수정

--- src/domains/widgets/components/banner-widget/BannerWrapper.tsx
+++ src/domains/widgets/components/banner-widget/BannerWrapper.tsx
@@ -19,7 +19,9 @@ export const renderImageBanner = (banner: Banner) => (
             const target = e.target as HTMLImageElement;
             target.style.display = 'none';
           }}
-          unoptimized
+          loading="eager"
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
         />
       </div>
     ) : (
