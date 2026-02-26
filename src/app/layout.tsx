e943408dea41f54cc5a9d2b3b8c92735c0d21f81
Taesu Kim <xotn1601@gmail.com>
2026-02-26 21:26:11 +0900

    카카오 광고 수정 삽입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,14 +49,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="image/svg+xml"
           fetchPriority="high"
         />
-        {/* Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MESEGFZZPF" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WWZCSPDV');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-MESEGFZZPF');`,
           }}
         />
         {/* Google AdSense */}
